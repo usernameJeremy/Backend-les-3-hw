94591049fcec7ff80e879cf32f1829e7b57213a5
usernameJeremy <jeremy_koster@outlook.com>
2022-09-16 14:49:47 +0200

    backend-les3-hw

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,12 @@ public class Main {
         String input;
 
         while (play) {
-            //scanner
+
             Scanner scanner = new Scanner(System.in);
-            //arrays
+
             Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
             String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
-            //scanner output
+
             System.out.println("type x to stop and type v to continue");
             input = scanner.nextLine();
             if (input.equalsIgnoreCase("x")){
@@ -23,12 +23,13 @@ public class Main {
             }else if(input.equalsIgnoreCase("v")){System.out.println("Please insert a number from 0 to 9");
                 int inputScanner = scanner.nextInt();
                 System.out.println();
-                //translator methode
+
                 TransLator translate = new TransLator(alphabetic, numeric);
                 String answer = translate.translate(inputScanner);
                 System.out.println(answer);}
             else {
                 System.out.println (ongeldig);
+
             }
         }
     }
